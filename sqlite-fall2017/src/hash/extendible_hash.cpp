17094aa8c855d17753abbdbfe983f3fcfe5fe633
yuyilei <yyl1352091742@gmail.com>
2018-04-30 23:07:55 +0800

    finish extendible hashing

--- sqlite-fall2017/src/hash/extendible_hash.cpp
+++ sqlite-fall2017/src/hash/extendible_hash.cpp
@@ -1,6 +1,6 @@
 #include <list>
 #include <iostream>
-#include <cstdio> 
+#include <cstdio>
 
 #include "hash/extendible_hash.h"
 #include "page/page.h"
@@ -13,10 +13,10 @@ namespace cmudb {
  */
 template <typename K, typename V>
 ExtendibleHash<K, V>::ExtendibleHash(size_t size) {
-    bucket_size_ = size ; 
-    gd = 0 ; 
-    bucket_counts = 1 ;
-    bs.emplace_back(new Bucket(0,0)) ;
+    bucket_size_ = size ;
+    gd = 0 ;                            // 初始化  gd 为0
+    bucket_counts = 1 ;                // 刚开始只有一个桶, 桶的数量：有元素的桶的数量 
+    bs.push_back(new Bucket(0,0)) ;
 }
 
 /*
@@ -26,7 +26,7 @@ ExtendibleHash<K, V>::ExtendibleHash(size_t size) {
  */
 template <typename K, typename V>
 size_t ExtendibleHash<K, V>::HashKey(const K &key) {
-    return std::hash<K>()(key) ; 
+    return std::hash<K>()(key) ;
 }
 
 /*
@@ -35,20 +35,20 @@ size_t ExtendibleHash<K, V>::HashKey(const K &key) {
  */
 template <typename K, typename V>
 int ExtendibleHash<K, V>::GetGlobalDepth() const {
-  std::unique_lock<std::mutex> lock(bucket_mutex) ; 
+  std::unique_lock<std::mutex> lock(bucket_mutex) ;
   return gd ;
 }
 
 /*
  * helper function to return local depth of one specific bucket
- * NOTE: you must implement this function in order to pass test
+ * NOTE: you must implement this function in order to pass test --> 检查不符合的index和不存在的index
  */
 template <typename K, typename V>
 int ExtendibleHash<K, V>::GetLocalDepth(int bucket_id) const {
-  std::unique_lock<std::mutex> lock(bucket_mutex) ; 
-  if ( bucket_id < 0 || bucket_id >= bs.size() || bs[bucket_id] == NULL ) 
-      return -1 ; 
-  return bs[bucket_id]->ld ; 
+  std::unique_lock<std::mutex> lock(bucket_mutex) ;
+  if ( bucket_id < 0 || bucket_id >= bs.size() || bs[bucket_id] == NULL )
+      return -1 ;
+  return bs[bucket_id]->ld ;
 }
 
 /*
@@ -56,7 +56,7 @@ int ExtendibleHash<K, V>::GetLocalDepth(int bucket_id) const {
  */
 template <typename K, typename V>
 int ExtendibleHash<K, V>::GetNumBuckets() const {
-  std::unique_lock<std::mutex> lock(bucket_mutex) ; 
+  std::unique_lock<std::mutex> lock(bucket_mutex) ;
   return bucket_counts ;
 }
 
@@ -65,19 +65,16 @@ int ExtendibleHash<K, V>::GetNumBuckets() const {
  */
 template <typename K, typename V>
 bool ExtendibleHash<K, V>::Find(const K &key, V &value) {
-  std::unique_lock<std::mutex> lock(bucket_mutex) ; 
-  size_t h = HashKey(key) ; 
-  if ( bs[h] == NULL ) 
-    return false ;  
-  auto b = bs[h] ; 
-  do {
-    if ( b->m.count(key) != 0 ) {
-      value = b->m[key] ; 
-      return true ; 
-    }
-    b = b->next ;  
-  } while ( b ) ; 
-  return false ; 
+  std::unique_lock<std::mutex> lock(bucket_mutex) ;
+  int h = BucketIndex(key) ;
+  if ( bs[h] == NULL )
+    return false ;
+  auto b = bs[h] ;
+  if ( b->m.count(key) != 0 ) {
+    value = b->m[key] ;
+    return true ;
+  }
+  return false ;
 }
 
 /*
@@ -86,22 +83,20 @@ bool ExtendibleHash<K, V>::Find(const K &key, V &value) {
  */
 template <typename K, typename V>
 bool ExtendibleHash<K, V>::Remove(const K &key) {
-  std::unique_lock<std::mutex> lock(bucket_mutex) ;  
-  size_t h = HashKey(key) ;
-  bool flag = false ; 
-  if ( bs[h] == NULL ) 
-    return false ; 
+  std::unique_lock<std::mutex> lock(bucket_mutex) ;
+  int h = BucketIndex(key) ;
+  if ( bs[h] == NULL )
+    return false ;
   auto b = bs[h] ;
-  do {
-    if ( b->m.count(key) != 0 ) {
-      b->m.erase(key) ; 
-      flag = true ; 
-    }
-    b = b->next ; 
-  } while ( b ) ; 
-  return flag ;
+  if ( b->m.count(key) != 0 ) {
+    b->m.erase(key) ;
+    return true ; 
+  }
+  return false ;
 }
 
+
+ 
 /*
  * insert <key,value> entry in hash table
  * Split & Redistribute bucket when there is overflow and if necessary increase
@@ -109,33 +104,75 @@ bool ExtendibleHash<K, V>::Remove(const K &key) {
  */
 template <typename K, typename V>
 void ExtendibleHash<K, V>::Insert(const K &key, const V &value) {
-  std::unique_lock<std::mutex> lock(bucket_mutex) ;    
-  size_t h = HashKey(key) ; 
-  if ( bs[h] == NULL ) {
-    bs[h] = std::make_shared<Bucket>(h,1) ; 
-    bucket_counts++ ; 
+  std::unique_lock<std::mutex> lock(bucket_mutex) ;
+  int h = BucketIndex(key) ;
+  if ( bs[h] == NULL ) {                                    
+    bs[h] = new Bucket(h,gd) ;
+    bucket_counts++ ;
   }
-  auto b = bs[h] ; 
+  auto b = bs[h] ;
   // 已存在，修改value
-  if ( b->m.count(key) != 0 ) {
-    b->m[key] = value ; 
-    return ; 
+  if ( b->m.find(key) != b->m.end() ) {
+    b->m[key] = value ;
+    return ;
   }
-  
-  b->m[key] = value ; 
-  if ( b->m.size() > bucket_size_ ) {
-    ; 
+
+  b->m.insert({key,value}) ; 
+  if ( b->m.size() <= bucket_size_ ) {          // 如果bucket 没满，直接插入后返回  
+    return ; 
   } 
+                                                  // 接下来的情况都是bucket满了的情况 
+  int oldsize = bs.size() ; 
+
+  struct Bucket *newb = new Bucket(0,b->ld) ;       // 127-146 : split桶  
+  while ( newb->m.empty() ) {
+    newb->ld++ ; 
+    b->ld++ ; 
+    for ( auto it = b->m.begin() ; it != b->m.end() ; ) {
+      if ( HashKey(it->first) & (1 << (newb->ld-1)) ) {              // 判断第ld位是否为1  
+        newb->id = HashKey(it->first) & ( (1<< newb->ld) -1 ) ; 
+        newb->m.insert(*it) ; 
+        it = b->m.erase(it) ; 
+      }
+      else {
+        it++ ; 
+      }
+    }
+    if ( b->m.empty() ) {                            // 如果全部分配到一个桶里面，说明还需要重新分配
+      b->m.swap(newb->m) ;                           // 此时，把新桶的ID给旧桶，在下一次循环中，新桶会被分配新的ID， 
+      b->id = newb->id ; 
+    }
+  }  
+  bucket_counts++ ; 
+
+
+  if ( newb->ld > gd ) {                         //  分配之后，localdepth大于globaldepth，要给bs增加容量  
+    bs.resize(oldsize * (1 << (newb->ld - gd))) ;          //  bs 的size需要增大的倍数 
+    gd = newb->ld ; 
+    for ( int i = 0 ; i < oldsize ; i++ ) {
+      if ( bs[i] == NULL ) 
+        continue ; 
+      for ( int j = i ; j < bs.size() ; j+= ( 1 << bs[i]->ld )) {    // 设置没有拓展的bucket和原bucket一样 
+        bs[j] = bs[i] ; 
+      } 
+    }
+    bs[b->id] = b ; 
+    bs[newb->id] = newb ; 
+  }
+  else {                                        // split之后，localdepth没超出globaldepth 
+    bs[b->id] = b ; 
+    bs[newb->id] = newb ; 
+  }
 }
 
 /*
- * 由hash值生成应存入的bucket的index 
- * 返回的值是 hashkey(key)的右边 gd位中1的个数  
+ * 由hash值生成应存入的bucket的index
+ * 返回的值是 hashkey(key)的右边 gd位中1的个数
  */
 template <typename K, typename V>
 int ExtendibleHash<K, V>::BucketIndex(const K &key) {
-  return HashKey(key) & (( 1 << gd ) - 1 ) ; 
-} 
+  return HashKey(key) & (( 1 << gd ) - 1 ) ;
+}
 
 template class ExtendibleHash<page_id_t, Page *>;
 template class ExtendibleHash<Page *, std::list<Page *>::iterator>;
--- sqlite-fall2017/src/include/hash/extendible_hash.h
+++ sqlite-fall2017/src/include/hash/extendible_hash.h
@@ -12,11 +12,11 @@
 #include <cstdlib>
 #include <vector>
 #include <string>
-#include <mutex> 
+#include <mutex>
 #include <map>
 #include <memory>
-#include <thread> 
-#include <chrono> 
+#include <thread>
+#include <chrono>
 
 #include "hash/hash_table.h"
 
@@ -27,11 +27,10 @@ class ExtendibleHash : public HashTable<K, V> {
 public:
   // constructor
   struct Bucket{
-    int id, ld ;                      // 分别是bucket的ID和local global 
-    std::map<K, V> m ;                // Bucket中的元素  
-    std::shared_ptr<struct Bucket> next ;          // 同一个组中桶（就是overflow了之后，新增的桶） 
+    int id, ld ;                      // 分别是bucket的ID和local global
+    std::map<K, V> m ;                // Bucket中的元素
     Bucket(int i, int d): id(i), ld(d) {}
-  }; 
+  };
 
   ExtendibleHash(size_t size);
   // helper function to generate hash addressing
@@ -44,15 +43,15 @@ public:
   bool Find(const K &key, V &value) override;
   bool Remove(const K &key) override;
   void Insert(const K &key, const V &value) override;
-  // 用来查找bucket的index 
-  int BucketIndex(const K &key) ;  
+  // 用来查找bucket的index
+  int BucketIndex(const K &key) ;
 
 private:
   // add your own member variables here
-  mutable std::mutex bucket_mutex ;                        // 保护共享数据，同时只有一个读写 
-  size_t bucket_size_ ;                    // 每个bucket中的最大数量 
-  int gd ;                                          // global depth 
-  std::vector<std::shared_ptr<Bucket>> bs ;                          // 桶的数组 
-  int bucket_counts ;                               // bucket 的数量 
+  mutable std::mutex bucket_mutex ;                        // 保护共享数据，同时只有一个读写
+  size_t bucket_size_ ;                    // 每个bucket中的最大数量
+  int gd ;                                          // global depth
+  std::vector<struct Bucket*> bs ;                          // 桶的数组
+  int bucket_counts ;                               // bucket 的数量
 };
 } // namespace cmudb
